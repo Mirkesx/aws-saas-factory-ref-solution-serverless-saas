b8c05cb2e76453ad7dcf551eb9506fbfd4464a8f
Mirkesx <cmarcos46@gmail.com>
2022-06-01 16:04:28 +0200

    bugfix: removing comments

--- deployment.sh
+++ deployment.sh
@@ -1,61 +1,61 @@
 #!/bin/bash -x
-# #Create CodeCommit repo
-# REGION=$(aws configure get region)
-# aws codecommit get-repository --repository-name aws-saas-factory-ref-serverless-saas
-# if [[ $? -ne 0 ]]; then
-#      echo "aws-saas-factory-ref-serverless-saas codecommit repo is not present, will create one now"
-#      CREATE_REPO=$(aws codecommit create-repository --repository-name aws-saas-factory-ref-serverless-saas --repository-description "Serverless saas reference architecture repository")
-#      echo $CREATE_REPO
-#      REPO_URL="codecommit::${REGION}://aws-saas-factory-ref-serverless-saas"
-#      git remote add cc $REPO_URL
-#      if [[ $? -ne 0 ]]; then
-#          echo "Setting url to remote cc"
-#          git remote set-url cc $REPO_URL
-#      fi    
-#      git push --set-upstream cc main
-# fi
+#Create CodeCommit repo
+REGION=$(aws configure get region)
+aws codecommit get-repository --repository-name aws-saas-factory-ref-serverless-saas
+if [[ $? -ne 0 ]]; then
+     echo "aws-saas-factory-ref-serverless-saas codecommit repo is not present, will create one now"
+     CREATE_REPO=$(aws codecommit create-repository --repository-name aws-saas-factory-ref-serverless-saas --repository-description "Serverless saas reference architecture repository")
+     echo $CREATE_REPO
+     REPO_URL="codecommit::${REGION}://aws-saas-factory-ref-serverless-saas"
+     git remote add cc $REPO_URL
+     if [[ $? -ne 0 ]]; then
+         echo "Setting url to remote cc"
+         git remote set-url cc $REPO_URL
+     fi    
+     git push --set-upstream cc main
+fi
 
 
 
-# #Deploying CI/CD pipeline
-# cd server/TenantPipeline/
-# npm install && npm run build 
-# cdk bootstrap  
-# cdk deploy 
+#Deploying CI/CD pipeline
+cd server/TenantPipeline/
+npm install && npm run build 
+cdk bootstrap  
+cdk deploy 
 
-# if [[ $? -ne 0 ]]; then
-#     echo "CDK"
-# fi
+if [[ $? -ne 0 ]]; then
+    echo "CDK"
+fi
 
-# # Deploying bootstrap
-# cd ../
+# Deploying bootstrap
+cd ../
 
-# DEFAULT_SAM_S3_BUCKET=$(grep s3_bucket samconfig-bootstrap.toml|cut -d'=' -f2 | cut -d \" -f2)
-# echo "aws s3 ls s3://$DEFAULT_SAM_S3_BUCKET"
-# aws s3 ls s3://$DEFAULT_SAM_S3_BUCKET
-# if [ $? -ne 0 ]; then
-#     echo "S3 Bucket: $DEFAULT_SAM_S3_BUCKET specified in samconfig-bootstrap.toml is not readable.
-#     So creating a new S3 bucket and will update samconfig-bootstrap.toml with new bucket name."
+DEFAULT_SAM_S3_BUCKET=$(grep s3_bucket samconfig-bootstrap.toml|cut -d'=' -f2 | cut -d \" -f2)
+echo "aws s3 ls s3://$DEFAULT_SAM_S3_BUCKET"
+aws s3 ls s3://$DEFAULT_SAM_S3_BUCKET
+if [ $? -ne 0 ]; then
+    echo "S3 Bucket: $DEFAULT_SAM_S3_BUCKET specified in samconfig-bootstrap.toml is not readable.
+    So creating a new S3 bucket and will update samconfig-bootstrap.toml with new bucket name."
     
-#     UUID=$(uuidgen | awk '{print tolower($0)}')
-#     SAM_S3_BUCKET=sam-bootstrap-bucket-$UUID
-#     aws s3 mb s3://$SAM_S3_BUCKET --region $REGION
-#     if [[ $? -ne 0 ]]; then
-#       echo "S3 MB"
-#     fi
-#     # Updating samconfig-bootstrap.toml with new bucket name
-#     ex -sc '%s/s3_bucket = .*/s3_bucket = \"'$SAM_S3_BUCKET'\"/|x' samconfig-bootstrap.toml
-# fi
+    UUID=$(uuidgen | awk '{print tolower($0)}')
+    SAM_S3_BUCKET=sam-bootstrap-bucket-$UUID
+    aws s3 mb s3://$SAM_S3_BUCKET --region $REGION
+    if [[ $? -ne 0 ]]; then
+      echo "S3 MB"
+    fi
+    # Updating samconfig-bootstrap.toml with new bucket name
+    ex -sc '%s/s3_bucket = .*/s3_bucket = \"'$SAM_S3_BUCKET'\"/|x' samconfig-bootstrap.toml
+fi
 
-# sam build -t bootstrap-template.yaml --use-container --region=$REGION
-# sam deploy --config-file samconfig-bootstrap.toml --region=$REGION --parameter-overrides AdminEmailParameter=$1 
+sam build -t bootstrap-template.yaml --use-container --region=$REGION
+sam deploy --config-file samconfig-bootstrap.toml --region=$REGION --parameter-overrides AdminEmailParameter=$1 
 
-# if [[ $? -ne 0 ]]; then
-#     echo "SAM deploy"
-# fi
+if [[ $? -ne 0 ]]; then
+    echo "SAM deploy"
+fi
 
-# # Start CI/CD pipepline which loads tenant stack
-# aws codepipeline start-pipeline-execution --name serverless-saas-pipeline 
+# Start CI/CD pipepline which loads tenant stack
+aws codepipeline start-pipeline-execution --name serverless-saas-pipeline 
 
 
 ADMIN_SITE_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name=='Serverless-SaaS-AdminAppBucket'].Value" --output text )
